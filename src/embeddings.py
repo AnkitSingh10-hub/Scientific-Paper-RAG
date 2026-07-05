98a5f4506defae3c585bc58dce789ad9f14565d8
Ankit Singh <59360110+AnkitSingh10-hub@users.noreply.github.com>
2026-07-06 01:36:30 +0545

    fix. added embedding types

--- src/embeddings.py
+++ src/embeddings.py
@@ -2,6 +2,7 @@ from huggingface_hub import login
 from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
 import os
+from abc import ABC, abstractmethod
 
 load_dotenv()
 
@@ -9,14 +10,80 @@ _token = os.getenv("RAG_TOKEN")
 if _token:
     login(token=_token)
 
-model = SentenceTransformer("BAAI/bge-small-en-v1.5")
+
+class Embedder(ABC):
+    @abstractmethod
+    def embed(self, chunks):
+        pass
+
+    @property
+    @abstractmethod
+    def model_name(self):
+        pass
 
 
-def embed_chunks(chunks):
-    embeddings = model.encode(
-        chunks,
-        normalize_embeddings=True,
-        show_progress_bar=True,
-    )
+class BGEEmbedding(Embedder):
+    def __init__(self):
+        self.model = SentenceTransformer("BAAI/bge-small-en-v1.5")
 
-    return embeddings
+    @property
+    def model_name(self):
+        return "bge-small-en-v1.5"
+
+    def embed(self, chunks):
+        return self.model.encode(
+            chunks,
+            normalize_embeddings=True,
+            show_progress_bar=True,
+        )
+
+
+class MiniLMEmbedding(Embedder):
+    def __init__(self):
+        self.model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+    @property
+    def model_name(self):
+        return "MiniLM-L6-v2"
+
+    def embed(self, chunks):
+        return self.model.encode(
+            chunks,
+            normalize_embeddings=True,
+            show_progress_bar=True,
+        )
+
+
+class MPNetEmbedding(Embedder):
+    def __init__(self):
+        self.model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+
+    @property
+    def model_name(self):
+        return "all-mpnet-base-v2"
+
+    def embed(self, chunks):
+        return self.model.encode(
+            chunks,
+            normalize_embeddings=True,
+            show_progress_bar=True,
+        )
+
+
+class E5Embedding(Embedder):
+    def __init__(self):
+        self.model = SentenceTransformer("intfloat/e5-base-v2")
+
+    @property
+    def model_name(self):
+        return "e5-base-v2"
+
+    def embed(self, chunks):
+
+        chunks = [f"passage: {c}" for c in chunks]
+
+        return self.model.encode(
+            chunks,
+            normalize_embeddings=True,
+            show_progress_bar=True,
+        )
